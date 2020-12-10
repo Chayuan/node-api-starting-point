ab325118784c798805d688793eddf83f6ddda475
Chayuan <gwenael.rob@gmail.com>
2020-12-10 14:50:43 +0100

    trigger heroku build

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export default (req: Request, res: Response, next: NextFunction): any => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
